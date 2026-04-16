e14fbfae8cc3ab9f0c16decf03f1cc899a55b2a2
mbafokou2-design <mbafokou2@gmail.com>
2026-04-16 16:21:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from routes.admin_routes import router as admin_router
 
 app = FastAPI(title="CommentWave API", version="1.0.0")
 
-# Allow React frontend to talk to this backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://commentwave-frontendhost.vercel.app"
+        "https://commentwave-frontendhost.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
